67c5170557c835a3d836a346090cff774b247a11
jademoslares <jade.moslares@gmail.com>
2024-04-23 20:03:29 -0400

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import styles from './App.module.css'
+import { Navbar } from './components/Navbar/Navbar'
+
+function App() {
+  return (
+    <>
+    <div className={styles.App}>
+      <Navbar/>
+    </div>
+    </>
+  )
+}
+
+export default App
