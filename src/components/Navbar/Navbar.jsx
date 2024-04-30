afb017df8236fb1d292eb3b52e72a5f8f2923ed2
jademoslares <jade.moslares@gmail.com>
2024-04-30 13:40:22 -0400

    resume link update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,34 +6,11 @@ export const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   const downloadFile = () => {
-    // Constructing the URL to the file
-    const fileUrl = `${window.location.origin}/assets/resume/JohnJadeMoslaresResume.docx`;
+    // Define the Google Docs link
+    const googleDocsLink = "https://docs.google.com/document/d/1ThsZsKsJbURFlNKl3UJmHBLlISnQV85O-vWj_6OQuOw/edit?usp=sharing";
 
-    fetch(fileUrl)
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.blob();
-        })
-        .then((blob) => {
-            // Create a URL for the blob
-            const fileURL = window.URL.createObjectURL(blob);
-            
-            // Create an <a> element to trigger the download
-            const link = document.createElement("a");
-            link.href = fileURL;
-            link.download = "JohnJadeMoslaresResume.docx";
-            
-            // Click the link to start the download
-            link.click();
-            
-            // Cleanup: Revoke the object URL
-            window.URL.revokeObjectURL(fileURL);
-        })
-        .catch((error) => {
-            console.error('Error downloading file:', error);
-        });
+    // Open the Google Docs link in a new tab
+    window.open(googleDocsLink, "_blank");
 };
 
 
@@ -71,7 +48,7 @@ export const Navbar = () => {
             <a href="#contact">Contacts</a>
           </li>
           <li>
-            <a onClick={downloadFile}>Resume</a>
+            <a href="#"onClick={downloadFile}>Resume</a>
           </li>
         </ul>
       </div>
